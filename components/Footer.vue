98ea8710c700b1d89a285be794a80d60d0756f8a
Christian Calderon <chris.calderon.ortega@gmail.com>
2021-02-26 12:04:39 -0700

    First Commit

---
+++ components/Footer.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="bg-gray-900 p-4 justify-center items-center text-center">
+      <p class="text-4lg">&copy; Copyright <span>Raptor Devs 2021</span> </p>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+p {
+    color: #FFFFFF
+}
+span{
+    font-weight: 800;
+    
+}
+</style>
---
+++ components/Header.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="bg-gray-900 flex p-4 justify-between">
+      <a href="" class="logo mr-auto"><img src="../assets/img/logo.png" alt=""></a>
+      <nav class="nav-menu justify-center items-center text-center">
+          <ul>
+              <li>
+                  <a href="" class="px-5">Home</a>
+              </li>
+              <li>
+                  <a href="" class="px-5">About Us</a>
+              </li>
+              <li>
+                  <a href="" class="px-5 orange_app">Contact</a>
+              </li>
+          </ul>
+      </nav>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.logo img{
+    max-width: 200px;
+}
+.nav-menu{
+    display: flex;
+    list-style: none;
+}
+.nav-menu li{
+    color: #f0eeee;
+    list-style: none;
+    display: inline-block;
+}
+.nav-menu a{
+    font-family: 'Roboto', sans-serif;
+    font-size: 18px;
+    @apply font-bold;
+}
+.nav-menu a:hover{
+    color: #EB6E15
+}
+
+</style>
